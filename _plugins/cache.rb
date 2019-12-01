7aa4982865a6c18831f611b3d74fe34b58b9a03e
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-12-01 16:04:29 -0500

    Refactor committee caching

--- _plugins/cache.rb
+++ _plugins/cache.rb
@@ -3,7 +3,7 @@ module Cache
     def generate(site)
       cache = {}
 
-      for collection in ['legislators', 'districts', 'committees'] do
+      for collection in ["legislators", "districts", "committees"] do
         item_by_id = {}
         for item in site.collections[collection].docs do
           id = item.id.delete_prefix("/#{collection}/")
@@ -12,35 +12,26 @@ module Cache
         cache["#{collection}_by_id"] = item_by_id
       end
 
-      for legislator in site.collections['legislators'].docs do
-        legislator.data['committees'] = []
+      for legislator in site.collections["legislators"].docs do
+        legislator.data["committees"] = []
       end
-      for committee in site.collections['committees'].docs do
+      legislators = cache["legislators_by_id"]
+      for committee in site.collections["committees"].docs do
         title = committee["title"]
-        legislators = cache['legislators_by_id']
-        if ['house', 'joint'].include? committee['chamber'] then
-          chair = legislators[committee['house_chair']]
-          chair.data['committees'].push({"role" => "Chair", "title" => title})
-          vice_chair = legislators[committee['house_vice_chair']]
-          vice_chair.data['committees'].push({"role" => "Vice-chair", "title" => title})
-          for member_id in committee['house_members'] do
-            member = legislators[member_id]
-            member.data['committees'].push({"role" => "Member", "title" => title})
-          end
-        end
-        if ['senate', 'joint'].include? committee['chamber'] then
-          chair = legislators[committee['senate_chair']]
-          chair.data['committees'].push({"role" => "Chair", "title" => title})
-          vice_chair = legislators[committee['senate_vice_chair']]
-          vice_chair.data['committees'].push({"role" => "Vice-chair", "title" => title})
-          for member_id in committee['senate_members'] do
-            member = legislators[member_id]
-            member.data['committees'].push({"role" => "Member", "title" => title})
+        for chamber in  ["house","senate"] do
+          if [chamber, "joint"].include? committee["chamber"] then
+            chair = legislators[committee["#{chamber}_chair"]]
+            chair.data["committees"].push({"role" => "Chair", "title" => title})
+            vice_chair = legislators[committee["#{chamber}_vice_chair"]]
+            vice_chair.data["committees"].push({"role" => "Vice-chair", "title" => title})
+            for member_id in committee["#{chamber}_members"] do
+              member = legislators[member_id]
+              member.data["committees"].push({"role" => "Member", "title" => title})
+            end
           end
         end
       end
-
-      site.data['cache'] = cache
+      site.data["cache"] = cache
     end
   end
 end
