61f89f55ecd3b7dc060c100b623cdb43a237be3d
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-12-01 13:11:23 -0500

    Add committees on leg pages

--- _plugins/cache.rb
+++ _plugins/cache.rb
@@ -2,6 +2,7 @@ module Cache
   class Generator < Jekyll::Generator
     def generate(site)
       cache = {}
+
       for collection in ['legislators', 'districts', 'committees'] do
         item_by_id = {}
         for item in site.collections[collection].docs do
@@ -10,6 +11,35 @@ module Cache
         end
         cache["#{collection}_by_id"] = item_by_id
       end
+
+      for legislator in site.collections['legislators'].docs do
+        legislator.data['committees'] = []
+      end
+      for committee in site.collections['committees'].docs do
+        title = committee["title"]
+        legislators = cache['legislators_by_id']
+        if ['house', 'joint'].include? committee['chamber'] then
+          chair = legislators[committee['house_chair']]
+          chair.data['committees'].push({"role" => "Chair", "title" => title})
+          vice_chair = legislators[committee['house_vice_chair']]
+          vice_chair.data['committees'].push({"role" => "Vice-chair", "title" => title})
+          for member_id in committee['house_members'] do
+            member = legislators[member_id]
+            member.data['committees'].push({"role" => "Member", "title" => title})
+          end
+        end
+        if ['senate', 'joint'].include? committee['chamber'] then
+          chair = legislators[committee['senate_chair']]
+          chair.data['committees'].push({"role" => "Chair", "title" => title})
+          vice_chair = legislators[committee['senate_vice_chair']]
+          vice_chair.data['committees'].push({"role" => "Vice-chair", "title" => title})
+          for member_id in committee['senate_members'] do
+            member = legislators[member_id]
+            member.data['committees'].push({"role" => "Member", "title" => title})
+          end
+        end
+      end
+
       site.data['cache'] = cache
     end
   end
