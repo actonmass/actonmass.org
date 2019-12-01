2dfe4183892c8841a474583d4cc5e84b3d35be1d
Florian Pagnoux <florian.pagnoux@gmail.com>
2019-12-01 13:27:24 -0500

    Refactor cache

--- _plugins/cache.rb
+++ _plugins/cache.rb
@@ -2,28 +2,14 @@ module Cache
   class Generator < Jekyll::Generator
     def generate(site)
       cache = {}
-
-      legislator_by_id = {}
-      for legislator in site.collections['legislators'].docs do
-        id = legislator.id.delete_prefix("/legislators/")
-        legislator_by_id[id] = legislator
+      for collection in ['legislators', 'districts', 'committees'] do
+        item_by_id = {}
+        for item in site.collections[collection].docs do
+          id = item.id.delete_prefix("/#{collection}/")
+          item_by_id[id] = item
+        end
+        cache["#{collection}_by_id"] = item_by_id
       end
-      cache['legislator_by_id'] = legislator_by_id
-
-      district_by_id = {}
-      for district in site.collections['districts'].docs do
-        id = district.id.delete_prefix("/districts/")
-        district_by_id[id] = district
-      end
-      cache['district_by_id'] = district_by_id
-
-      committee_by_id = {}
-      for committee in site.collections['committees'].docs do
-        id = committee.id.delete_prefix("/committees/")
-        committee_by_id[id] = committee
-      end
-      cache['committee_by_id'] = committee_by_id
-
       site.data['cache'] = cache
     end
   end
