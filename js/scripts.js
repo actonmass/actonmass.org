75f5884918d0d7962f61d449339a49bda5d946ac
Jordan Williams <jordanmswilliams@gmail.com>
2020-01-11 14:11:21 -0500

    "issues page responsive"

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-
+/* tab section for rep page */
 const tabs = document.querySelectorAll('[data-tab-target]')
 const tabContents = document.querySelectorAll('[data-tab-content]')
 
@@ -15,3 +15,23 @@ tabs.forEach(tab => {
     target.classList.add('active')
   })
 })
+
+/* accordion for common questions */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
