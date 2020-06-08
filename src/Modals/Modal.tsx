de510ea3fe051b3e1f010c2df56e579a97781322
Florian Pagnoux <florian.pagnoux@gmail.com>
2020-06-07 23:45:55 -0400

    Add modal

--- src/Modals/Modal.tsx
+++ src/Modals/Modal.tsx
@@ -1,12 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Modal from "react-modal";
 
 type Props = {
   txt: string;
 };
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
 function ContactLegModal({ txt }: Props) {
-  return <a class="btn">{txt}</a>;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <>
+      <a className="btn" onClick={() => setIsOpen(true)}>
+        {txt}
+      </a>
+      <Modal
+        isOpen={modalIsOpen}
+        // onAfterOpen={afterOpenModal}
+        onRequestClose={() => setIsOpen(false)}
+        // style={customStyles}
+        // contentLabel="Example Modal"
+        className="contact-leg-modal modal"
+        overlayClassName="modal-overlay"
+      >
+        <div className="modal-header">
+          <i class="fas fa-times fa-2x" onClick={() => setIsOpen(false)}></i>
+        </div>
+
+        <div class="vbox">
+          <a className="btn" onClick={() => setIsOpen(true)}>
+            Call your rep now
+          </a>
+          <a className="btn" onClick={() => setIsOpen(true)}>
+            Email your rep now
+          </a>
+          <a className="btn" onClick={() => setIsOpen(true)}>
+            Send your rep a tweet
+          </a>
+        </div>
+      </Modal>
+    </>
+  );
 }
 
 function renderModal(targetID: string, data: Props) {
