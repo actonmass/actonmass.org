bd93e5f245d717398539db88d25a56aac8437d7d
Florian Pagnoux <florian.pagnoux@gmail.com>
2020-06-28 19:36:31 -0400

    Fix and test interpolation

--- src/Modals/evalScripts.ts
+++ src/Modals/evalScripts.ts
@@ -1,7 +1,7 @@
 import doT from "dot";
 import _ from "lodash";
 
-import { Leg, Bill, EmailScript, Scripts } from "../types";
+import { Scripts } from "../types";
 
 type StringTree = { [key: string]: string | StringTree };
 
@@ -11,11 +11,11 @@ doT.templateSettings.strip = false;
 export function evalScripts(scripts: Scripts, params: { [key: string]: Object }): Scripts {
   const param_names = _.keys(params);
   const paramList = param_names.map((name) => params[name]);
-  doT.templateSettings.varname = param_names.join(", ");
+  doT.templateSettings.varname = param_names.join(",");
   function evalScriptsRec(tree: StringTree) {
     return _.mapValues(tree, (child) => {
       if (_.isString(child)) {
-        return doT.template(child)(paramList);
+        return doT.template(child)(...paramList);
       }
       return evalScriptsRec(child);
     });
