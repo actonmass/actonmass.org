226381f7611b98c6c743bd5f56f41d0a41ca933d
Florian Pagnoux <florian.pagnoux@gmail.com>
2020-06-29 00:13:14 -0400

    Cleanup

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 const tabs = document.querySelectorAll("[data-tab-target]");
 const tabContents = document.querySelectorAll("[data-tab-content]");
 
-tabs.forEach(tab => {
+tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.tabTarget);
-    tabContents.forEach(tabContent => {
+    tabContents.forEach((tabContent) => {
       tabContent.classList.remove("active");
     });
-    tabs.forEach(tab => {
+    tabs.forEach((tab) => {
       tab.classList.remove("active");
     });
     tab.classList.add("active");
@@ -21,7 +21,7 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
@@ -34,44 +34,4 @@ for (i = 0; i < acc.length; i++) {
       panel.style.display = "block";
     }
   });
-}
-
-/* script for pop-up homepage volunteer forms */
-
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
-
-/* script for pop-up homepage act now forms */
-
-function openForm1() {
-  document.getElementById("myForm1").style.display = "block";
-}
-
-function closeForm1() {
-  document.getElementById("myForm1").style.display = "none";
-}
-
-/* script for pop-up forms */
-
-function openForm2() {
-  document.getElementById("myForm2").style.display = "block";
-}
-
-function closeForm2() {
-  document.getElementById("myForm2").style.display = "none";
-}
-
-/* script for pop-up forms */
-
-function openForm3() {
-  document.getElementById("myForm3").style.display = "block";
-}
-
-function closeForm3() {
-  document.getElementById("myForm3").style.display = "none";
 }
