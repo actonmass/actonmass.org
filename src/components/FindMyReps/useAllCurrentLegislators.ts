f0757191f66791b99842fae4423365a39b89fa22
Florian Pagnoux <florian.pagnoux@gmail.com>
2021-04-13 23:04:41 -0400

    Simplify leg query

--- src/components/FindMyReps/useAllCurrentLegislators.ts
+++ src/components/FindMyReps/useAllCurrentLegislators.ts
@@ -2,50 +2,24 @@ import { useStaticQuery, graphql } from "gatsby";
 
 import { flattenQueryResult } from "../../utils";
 
-type RawLeg = {
-  aom_id: string;
+type Leg = {
   first_name: string;
   last_name: string;
-  district: string;
   href: string;
-};
-
-type Leg = RawLeg & {
   chamber: "house" | "senate";
 };
 
 export default function useAllCurrentLegislators(): Leg[] {
-  const data = useStaticQuery(query);
-  const rawLegs = flattenQueryResult(data) as RawLeg[];
-
-  return rawLegs.map((leg) => ({
-    ...leg,
-    // TODO: ideally this should be done server-side when generating the node
-    chamber: leg.district.startsWith("house") ? "house" : "senate",
-    href: `/legislators/${leg.aom_id}`,
-  }));
-}
-
-const query = graphql`
-  query getCurrentLegislators {
-    allMarkdownRemark(
-      sort: { fields: frontmatter___last_name }
-      filter: {
-        parent: {}
-        fileAbsolutePath: { regex: "/legislators/" }
-        frontmatter: { end_date: { in: [null, ""] } }
-      }
-    ) {
-      nodes {
-        frontmatter {
-          aom_id
+  return useStaticQuery(graphql`
+    query legislators {
+      allLegislators(filter: { end_date: { eq: "" } }) {
+        nodes {
+          href
           first_name
           last_name
-          district
-          end_date
+          chamber
         }
-        id
       }
     }
-  }
-`;
+  `).allLegislators.nodes;
+}
