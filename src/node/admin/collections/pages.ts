cda636a3a2c72f90e6c4ac6e6a0cdbafae33e696
Florian Pagnoux <florian.pagnoux@gmail.com>
2021-04-18 23:36:30 -0400

    Add layout to pages in CMS

--- src/node/admin/collections/pages.ts
+++ src/node/admin/collections/pages.ts
@@ -20,18 +20,30 @@ export default {
       required: false,
     },
     {
+      label: "Layout",
+      name: "layout",
+      widget: "select",
+      options: [
+        {
+          label: "Default",
+          value: "default",
+        },
+        {
+          label: "Minimal",
+          value: "base",
+        },
+      ],
+      default: "default",
+      hint:
+        "Default is similar to a blog post. Minimal is more bare-bone and usually requires you to add Section blocks.",
+    },
+    {
       label: "Body",
       name: "body",
       widget: "markdown",
       required: false,
       hint:
         "An empty body means that the page is either empty or rendered by a custom layout (and can't be edited here).",
-    },
-    {
-      label: "Layout",
-      name: "layout",
-      widget: "hidden",
-      default: "default",
     },
   ],
 };
