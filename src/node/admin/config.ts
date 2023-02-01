aebbb9c12e8e17e75b1f03230659c2f7153a764f
Florian Pagnoux <florian.pagnoux@gmail.com>
2023-02-01 11:09:23 -0500

    Change target branch to allow Erin to update text

--- src/node/admin/config.ts
+++ src/node/admin/config.ts
@@ -15,7 +15,8 @@ import {
 export default {
   backend: {
     name: "git-gateway",
-    branch: "main",
+    branch: "nkleinmann-campaign-page", // TODO: Switch back to main once the branch is ready to merge
+    // branch: "main",
     // publish_mode: editorial_workflow // Deactivate on next since we are still collection data and don't want to go through a PR for each change
   },
   local_backend: true,
