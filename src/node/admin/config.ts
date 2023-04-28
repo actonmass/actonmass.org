06369464892ea9a1c9827ce98e6dd0baa64ec0a5
nkleinmann <nekleinmann@gmail.com>
2023-04-28 15:06:23 -0400

    revert temporary change

--- src/node/admin/config.ts
+++ src/node/admin/config.ts
@@ -15,8 +15,7 @@ import {
 export default {
   backend: {
     name: "git-gateway",
-    branch: "nkleinmann-campaign-page", // TODO: Switch back to main once the branch is ready to merge
-    // branch: "main",
+    branch: "main",
     // publish_mode: editorial_workflow // Deactivate on next since we are still collection data and don't want to go through a PR for each change
   },
   local_backend: true,
